cd0be8581df6ffe23cac5c093c73013bb995412d
unknown <jehad.ispita@gmail.com>
2023-09-25 15:26:41 +0300

    create laravel project

---
+++ website/index.php
@@ -0,0 +1,23 @@
+<?php  
+class sumiton(){
+    public $number1=5;
+    public $number2=10;
+     
+    function full_number(){
+        return $this->number1+$this->number2
+    }
+
+}
+
+$sumiton=new number;
+echo $sumiton->full_number();
+
+
+ 
+
+
+
+
+
+?> 
+ 
